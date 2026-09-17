b8e4dec63c9e0b38a70f1fec61d2ea0e71ef655f
Miguel <magvegacruz@uamv.edu.ni>
2026-09-17 14:46:47 -0600

    Main por marcelo

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from seleccionar_ruta import seleccionar_ruta, ingresar_cantidad_pasajeros
-from tipo_descuento import determinar_tipo_pasajero
+from rutas import chooseRoutes, insertpassengerAmount
+from tipo_Descuento import determinar_tipo_pasajero
 from costo_resumen import finalizar_reserva
 
 if __name__ == "__main__":
     print("=== Gestión de reservas - Empresa de transporte ===")
-    ruta, precio = seleccionar_ruta()
-    cantidad = ingresar_cantidad_pasajeros()
+    ruta, precio = chooseRoutes()
+    cantidad = insertpassengerAmount()
     tipo, porcentaje = determinar_tipo_pasajero()
     finalizar_reserva(ruta, precio, cantidad, tipo, porcentaje)

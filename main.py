f6c03b9681ee9706cc81733e9390b93c35752db3
Miguel <magvegacruz@uamv.edu.ni>
2026-09-17 14:53:50 -0600

    Main de marcelo en ingles

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from rutas import chooseRoutes, insertpassengerAmount
-from tipo_Descuento import determinar_tipo_pasajero
+from tipo_Descuento import detPassType
 from costo_resumen import finalizar_reserva
 
 if __name__ == "__main__":
     print("=== Gestión de reservas - Empresa de transporte ===")
     ruta, precio = chooseRoutes()
     cantidad = insertpassengerAmount()
-    tipo, porcentaje = determinar_tipo_pasajero()
+    tipo, porcentaje = detPassType()
     finalizar_reserva(ruta, precio, cantidad, tipo, porcentaje)
